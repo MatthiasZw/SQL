77a701aebec08ef4899e71e6b41ecd790c65ac25
MatthiasZw <84776864+MatthiasZw@users.noreply.github.com>
2021-10-15 13:31:51 +0200

    Unterabfragen und Übungen (Freitag)

--- SQL/Unterabfragen.sql
+++ SQL/Unterabfragen.sql
@@ -49,16 +49,60 @@ select * from artikel
 	-- Gesucht sind die HAmburger Lieferanten die öfter geliefert haben wie blaue artikel geliefert wurden.
 
 
+	select lname from lieferant where lstadt ='hamburg' and  lnr in( select lnr from lieferung group by lnr having count(*) > (select count (*) from lieferung where anr in(select anr from artikel where farbe='blau'))); 
+	
+	--<-- stimmt scheinbar jetz
+
+
+	-- Gesucht sind die namen und Lagerorte der Artikel deren letzte Lieferung an dem Tag erfolgt ist, an dem auch artikel 'A02' letztmalig geliefert wurde
+
+	insert into lieferung values('L02', 'A05', 500, '09.08.1990');
+
+	
+
+	 select aname, astadt from artikel where anr in(select anr from lieferung group by anr having max(ldatum) in(select max(ldatum) from lieferung where anr='A02'));
+
+	 delete lieferung where datepart(yyyy, ldatum) > 1990 or lmenge = 500;
+	
+	select * from lieferung;
+
+	delete lieferant where lnr > 'L05';
 
 
 
-	select lname from lieferant where lstadt ='hamburg ' and  lnr in( select lnr from lieferung where (select count(lnr) from lieferung) > (select count (anr) from lieferung where anr in(select anr from artikel where farbe='blau')))
+	--Gesucht sind nummer, name und Wohnort der Lieferanten die jeden artikel geliefert haben
+
+	select lnr, lname, lstadt from lieferant where lnr in(select lnr from lieferung group by lnr having count(distinct anr) = (select count(anr) from artikel));
+
+
+	-- Gesucht ist das Liefedatum, wo hamburger Lierferanten in deren Namen ein i oder ein l vorkommt, rote oder blaue artikel mit einem gewicht über 15 gramm geliefert haben
+
+
+
+	select anr from artikel where (farbe='rot' or farbe= 'blau') and Gewicht > 15;
+
+    select lnr from lieferant where lname like '%i%' or  lname like '%l%');
+
+	select ldatum from lieferung where anr in(select anr from artikel where (farbe='rot' or farbe= 'blau') and Gewicht > 15);
+
+
+
+	select ldatum from lieferung where ldatum in (select ldatum from lieferung where anr in(select anr from artikel where (farbe='rot' or farbe= 'blau') and Gewicht > 15) and  lnr in(select lnr from lieferant where lname like '%i%' or  lname like '%l%' and lstadt = 'hamburg'));             
+
+
+
+	-- Gesucht sind die Artikel mit einer Lagermenge von mindestens 500 stück die öfters geliefert wurden 
+	-- wie LIeferanten aus Ludwigshafen im August 1990 geliefert haben
+
+	--- Anzahl  aller Artiklellieferungen aus ludwigshafen im august
+
+	select count(distinct anr) from lieferung where lnr in(select lnr from lieferant where lstadt = 'Ludwigshafen') and ldatum between '1.8.1990' and '31.8.1990'; 
+
+
+	select aname from artikel where amenge > 500 and anr in (select anr from lieferung group by anr having count(lnr)  > (select count(anr) from lieferung where lnr in(select lnr from lieferant where lstadt = 'Ludwigshafen') and ldatum between '1.8.1990' and '31.8.1990')); 
+
+
 	
-	
-	
-	-- haben blaue artikel geliefert: 
-	
-	lnr in (select lnr from lieferung where anr in(select anr from artikel where farbe='blau'));
 
 
 	
